480f7d144cdf860538663417e33241065b206220
Sachin-1616 <113449196+Sachin-1616@users.noreply.github.com>
2022-10-09 14:54:44 +0530

    My Portfolio

---
+++ portfolio.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+// echo $_GET["name1"]; 
+// echo $_GET["name2"]; 
+// echo $_GET["gmail"]; 
+// echo $_GET["txtarea"]; 
+$a=$_GET['name1'];
+$b=$_GET['name2'];
+$c=$_GET['email'];
+$d=$_GET['txtarea'];
+echo $a,$b,$c,$d;
+?>
+</body>
+</html>
